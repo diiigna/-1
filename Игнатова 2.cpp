e7990d76771fcdb3a5f02f00e7e14fc8b249788f
diiigna <151121497+diiigna@users.noreply.github.com>
2023-11-17 10:34:34 +0300

    Лабораторная 2

---
+++ Игнатова 2.cpp
@@ -0,0 +1,20 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+
+	int num1, num2;
+	cout << "Введите первое число:";
+	cin >> num1;
+	cout << "Введите второе число:";
+	cin >> num2;
+
+	int sredneearifm = (num1 + num2) / 2;
+	cout << "Среднее арифметическое этих чисел:" << sredneearifm << endl;
+
+	return 0;
+
+}
