a1114dc317c41525b803e8d31e53593fb9e392fe
diiigna <151121497+diiigna@users.noreply.github.com>
2023-11-17 10:53:15 +0300

    Практика 3

---
+++ ignatova 3.cpp
@@ -0,0 +1,23 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+	int start, end;
+	int sum = 0;
+	cout << "Введите начало диапазона:";
+	cin >> start;
+	cout << "Введите конец диапазона:";
+	cin >> end;
+	for (int i = start; i <= end; i++) {
+		if (i % 2 == 0) {
+			sum += i;
+		}
+	}
+	cout << "Сумма всех четных чисел в заданном диапазоне:" << sum << endl;
+	return 0;
+
+}
+
