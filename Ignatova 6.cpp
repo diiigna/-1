257319c89caf17b2c8043b5dc188f7bb4f39e599
diiigna <151121497+diiigna@users.noreply.github.com>
2023-11-17 12:12:28 +0300

    Практика 6

---
+++ Ignatova 6.cpp
@@ -0,0 +1,18 @@
+﻿
+#include <iostream>
+using namespace std;
+int main()
+{
+	setlocale(0, "");
+	int num1;
+	cout << "Введите число:";
+	cin >> num1;
+	cout << "делитель числа:" << num1 << ":";
+	for (int i = 1; i <= num1; ++i) {
+		if (num1 % i == 0) {
+			cout << i << ", ";
+		}
+	}
+	cout << endl;
+	return 0;
+}
